c66f31206219f287849baaeeeeb1dc2490e4cd1a
fathy <fathyatefbaiomy99@gmail.com>
2022-03-01 08:56:55 +0200

    initialized bmi result page

---
+++ lib/BMIResult.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BMIResult extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'bmi result',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:bmi_calculator_app/BMIResult.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BMIResult());
 }
 
 class MyApp extends StatefulWidget {
